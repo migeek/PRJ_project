ea17cd312e6e8730ca3c513e403c9b34ad6e67b1
Abdul <arafeik@calpoly.edu>
2020-06-13 22:55:44 -0700

    Removing server from this branch

--- FamilyVoice/website/config.py
+++
@@ -1,8 +0,0 @@
-from app import app
-import os
-import jinja2
-thisDir = os.path.abspath(os.path.dirname(__file__))
-
-my_loader = jinja2.ChoiceLoader([jinja2.FileSystemLoader([os.path.join(thisDir, 'templates')])])
-app.jinja_loader = my_loader
-
